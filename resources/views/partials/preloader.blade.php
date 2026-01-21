54aeb4f6653ea6ce4b03f4ce7d821457678fac45
Frank <francito20al@gmail.com>
2026-01-21 08:46:16 -0500

    Initial commit 03

--- resources/views/partials/preloader.blade.php
+++ resources/views/partials/preloader.blade.php
@@ -1,13 +1,13 @@
 <div class="preloader" id="preloader">
 	<div class="preloader">
 		<div class="waviy position-relative">
-			<span class="d-inline-block">I</span>
-			<span class="d-inline-block">S</span>
-			<span class="d-inline-block">L</span>
-			<span class="d-inline-block"></span>
-			<span class="d-inline-block">M</span>
-			<span class="d-inline-block">T</span>
+			<span class="d-inline-block">A</span>
+			<span class="d-inline-block">R</span>
+			<span class="d-inline-block">R</span>
 			<span class="d-inline-block">O</span>
+			<span class="d-inline-block">C</span>
+			<span class="d-inline-block">E</span>
+			<span class="d-inline-block">R</span>
 		</div>
 	</div>
 </div>
